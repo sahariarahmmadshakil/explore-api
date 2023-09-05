bc54d052c0152dafa3e779ffad69bab0f983d10e
sahariarahmmad <info.sashakil@gmail.com>
2023-09-06 00:22:53 +0600

    added data from server

--- api.js
+++ api.js
@@ -1,1 +1,8 @@
-console.log("Api ")
+const loadPhone = async () => {
+    const res =await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+    const phoneInfo = await res.json();
+    const phone = phoneInfo.data;
+    console.log(phone);
+
+}
+loadPhone()
